b478143b11ab2630d026eceaaa8df9611fb98cf1
mihanchik94 <mihanchik94@gmail.com>
2022-03-07 19:53:29 +0300

    3.1. Прерывание блокированной нити. [#267413]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -12,7 +12,6 @@ public class ConsoleProgress implements Runnable  {
         try {
             Thread.sleep(500);
         } catch (InterruptedException e) {
-            e.printStackTrace();
             Thread.currentThread().interrupt();
         }
         }
