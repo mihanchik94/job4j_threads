ecf425abfc090833376f40fbd4d72a76e20ed2a3
mihanchik94 <mihanchik94@gmail.com>
2022-03-09 21:54:25 +0300

    Immutable и не Immutable классы, знакомство

--- src/main/java/ru/job4j/Cache.java
+++ src/main/java/ru/job4j/Cache.java
@@ -1,12 +1,25 @@
 package ru.job4j;
 
-public final class Cache {
-    private static Cache cache;
+import net.jcip.annotations.ThreadSafe;
 
-    public static synchronized Cache instOf() {
-        if (cache == null) {
-            cache = new Cache();
-        }
-        return cache;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@ThreadSafe
+public class Cache {
+    private final ConcurrentHashMap<Integer, String> dic = new ConcurrentHashMap<>();
+    private final AtomicInteger ids = new AtomicInteger();
+
+    public Cache() {
+        dic.put(ids.incrementAndGet(), "Petr Arsentev");
+        dic.put(ids.incrementAndGet(), "Ivan Ivanov");
+    }
+
+    public void add(String name) {
+        dic.put(ids.incrementAndGet(), name);
+    }
+
+    public boolean contains(String name) {
+        return dic.contains(name);
     }
 }
