94ef66053cdd8e8d93498377ab6de31850053f04
mihanchik94 <mihanchik94@gmail.com>
2022-03-12 15:06:42 +0300

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/cas/CASCount.java
@@ -0,0 +1,26 @@
+package ru.job4j.cas;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>();
+
+    public void increment() {
+        int ref;
+        int next;
+        do {
+            ref = count.get();
+            next = ref++;
+        } while (!count.compareAndSet(ref, next));
+    }
+
+    public int get() {
+        if (count.get() == null) {
+            throw new UnsupportedOperationException("Count is not impl.");
+        }
+        return count.get();
+    }
+}
