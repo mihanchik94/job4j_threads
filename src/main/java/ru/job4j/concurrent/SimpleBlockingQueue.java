3723162242c8f3e00537fbe463968ca64cf0c29a
mihanchik94 <mihanchik94@gmail.com>
2022-03-13 09:44:55 +0300

    1. Реализовать шаблон Producer Consumer. [#1098 #274300] (r.1.01)

--- src/main/java/ru/job4j/concurrent/SimpleBlockingQueue.java
+++ src/main/java/ru/job4j/concurrent/SimpleBlockingQueue.java
@@ -34,8 +34,9 @@ public class SimpleBlockingQueue<T> {
         while (queue.isEmpty()) {
             wait();
         }
+        T result = queue.poll();
         notifyAll();
-        return queue.poll();
+        return result;
     }
 
     public int getLimit() {
