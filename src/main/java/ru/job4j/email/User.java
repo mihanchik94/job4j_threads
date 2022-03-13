02584c4380b251897fdf2e154377d7b619e06472
mihanchik94 <mihanchik94@gmail.com>
2022-03-13 12:27:27 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/email/User.java
@@ -0,0 +1,29 @@
+package ru.job4j.email;
+
+public class User {
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "name='"
+                + name + '\''
+                + ", email='"
+                + email + '\''
+                + '}';
+    }
+}
