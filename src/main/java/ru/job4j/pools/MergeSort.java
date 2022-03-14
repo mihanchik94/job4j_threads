6dbcb2cf1fc059bd883c3ec55139657ccfec953f
mihanchik94 <mihanchik94@gmail.com>
2022-03-14 20:03:03 +0300

    checkStyle

--- src/main/java/ru/job4j/pools/MergeSort.java
+++ src/main/java/ru/job4j/pools/MergeSort.java
@@ -1,28 +1,31 @@
 package ru.job4j.pools;
 
+/**
+ * при следующем условии, массив из одного элемента
+ * делить нечего, возвращаем элемент
+ * попали сюда, значит в массиве более одного элемента
+ * находим середину
+ * объединяем отсортированные части
+ * Метод объединения двух отсортированных массивов
+ */
+
+
 public class MergeSort {
     public static int[] sort(int[] array) {
         return sort(array, 0, array.length - 1);
     }
 
     private static int[] sort(int[] array, int from, int to) {
-        // при следующем условии, массив из одного элемента
-        // делить нечего, возвращаем элемент
         if (from == to) {
             return new int[] {array[from]};
         }
-        // попали сюда, значит в массиве более одного элемента
-        // находим середину
         int mid = (from + to) / 2;
-        // объединяем отсортированные части
         return merge(
                 sort(array, from, mid),
-                // сортируем правую часть
                 sort(array, mid + 1, to)
         );
     }
 
-    // Метод объединения двух отсортированных массивов
     public static int[] merge(int[] left, int[] right) {
         int li = 0;
         int ri = 0;
