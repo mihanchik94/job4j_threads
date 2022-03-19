457ad9f0036a4417762d318e32c5b4755968f056
mihanchik94 <mihanchik94@gmail.com>
2022-03-19 23:49:51 +0300

    Знакомство с CompletableFuture

---
+++ src/main/java/ru/job4j/asynchrony/RunAsyncExample.java
@@ -0,0 +1,104 @@
+package ru.job4j.asynchrony;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+public class RunAsyncExample {
+    private static void iWork() throws InterruptedException {
+        int count = 0;
+        while (count < 10) {
+            System.out.println("Я  работаю");
+            TimeUnit.SECONDS.sleep(1);
+            count++;
+        }
+    }
+    public static CompletableFuture<Void> goToTrash() {
+        return CompletableFuture.runAsync(
+                () -> {
+                    System.out.println("Сын: Мам/Пам, я пошел выносить мусор");
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Сын: Мам/Пап, я вернулся!");
+                }
+                );
+    }
+
+    public static void runAsyncExample() throws Exception {
+        CompletableFuture<Void> gtt = goToTrash();
+        iWork();
+    }
+
+    public static void thenRunExample() throws Exception {
+        CompletableFuture<Void> gtt = goToTrash();
+        gtt.thenRun(
+                () -> {
+                    int count = 0;
+                    while (count < 3) {
+                        System.out.println("Сын: я мою руки");
+                        try {
+                            TimeUnit.MILLISECONDS.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        count++;
+                    }
+                    System.out.println("Сын: Я помыл руки");
+                }
+        );
+        iWork();
+    }
+
+    public static CompletableFuture<Void> washHands(String name) {
+        return CompletableFuture.runAsync(
+                () -> {
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(name + " моет руки");
+                }
+        );
+    }
+
+    public static void allOfExample() throws Exception {
+        CompletableFuture<Void> all = CompletableFuture.allOf(
+                washHands("Мама"), washHands("Папа"),
+                washHands("Ваня"), washHands("Боря")
+        );
+        TimeUnit.SECONDS.sleep(3);
+    }
+
+    public static CompletableFuture<String> whoWashHands(String name) {
+        return CompletableFuture.supplyAsync(
+                () -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return name + " моет руки";
+        });
+    }
+
+    public static void anyOfExample() throws Exception {
+        CompletableFuture<Object> first = CompletableFuture.anyOf(
+                whoWashHands("Папа"), whoWashHands("Мама"),
+                whoWashHands("Ваня"), whoWashHands("Боря")
+        );
+        System.out.println("Кто сейчас моет руки?");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println(first.get());
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        //runAsyncExample();
+        //thenRunExample();
+        //allOfExample();
+        anyOfExample();
+    }
+}
